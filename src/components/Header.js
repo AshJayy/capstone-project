7c0382a48a52d29d7848348ee6e47d65b06e0c0c
AshJayy <ashwinie.ja@gmail.com>
2023-01-17 20:22:00 +0530

    components addwd to App.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,11 @@
 import React from "react";
+import Nav from "./Nav"
 
 function Header(){
     return(
-        <header></header>
+        <header>
+            <Nav />
+        </header>
     );
 }
 
