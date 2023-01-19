76a0286affb24bfc41d05add39a46ceaebcc0d54
AshJayy <ashwinie.ja@gmail.com>
2023-01-19 23:22:38 +0530

    Hero initialization

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,8 +1,18 @@
 import React from "react";
+import heroImg from './assets/img/hero_img.png';
 
 function Hero(){
     return(
-        <div></div>
+        <section id="hero">
+            <div>
+                <h1>Little Lemon</h1>
+                <h2>Chicago</h2>
+                <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist</p>
+            </div>
+            <div>
+                <img src={heroImg} alt="hero image" />
+            </div>
+        </section>
     );
 }
 
